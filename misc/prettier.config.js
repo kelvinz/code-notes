48a32363a12188fad1284dca20f0acf6a3fd77b1
kelvin zhao <takingovertheworld@gmail.com>
2021-04-09 08:08:46 +0800

    ✨ 210409

---
+++ misc/prettier.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	printWidth: 80,
+	tabWidth: 4,
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+	quoteProps: "consistent",
+	jsxSingleQuote: true,
+	trailingComma: "es5",
+	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	arrowParens: "avoid",
+	htmlWhitespaceSensitivity: "css",
+	vueIndentScriptAndStyle: false,
+	endOfLine: "lf"
+}
