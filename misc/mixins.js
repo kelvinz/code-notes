bff6848a52f9ae5b529bc5987b7de71ce1fd5426
kelvin zhao <takingovertheworld@gmail.com>
2024-08-12 20:47:40 +0800

    ✨ 240812

--- misc/mixins.js
+++ misc/mixins.js
@@ -188,4 +188,11 @@ export const isEmpty = ( value ) => {
 
 
 
+// capitalize the first letter of a string
+export const capitalize = ( str ) => {
+	return str.charAt( 0 ).toUpperCase() + str.slice( 1 )
+}
+
+
+
 }
