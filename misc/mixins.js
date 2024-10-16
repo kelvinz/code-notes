96f660ec06eede87c18e86fe25df2a9d2b5e213e
kelvin zhao <takingovertheworld@gmail.com>
2024-10-16 09:15:53 +0800

    ✨ 241016

--- misc/mixins.js
+++ misc/mixins.js
@@ -262,3 +262,10 @@ export const getAverage = ( numbers ) => {
 
 
 
+// convert RGB to HEX
+export const rgbToHex = ( r, g, b ) => {
+	return "#" + ( ( 1 << 24 ) + ( r << 16 ) + ( g << 8 ) + b ).toString( 16 ).slice( 1 )
+}
+
+
+
