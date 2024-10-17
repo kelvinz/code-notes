e9ef67db5703b3e3ee2e3dbd4d44d033c786a643
kelvin zhao <takingovertheworld@gmail.com>
2024-10-17 21:33:13 +0800

    ✨ 241017

--- misc/mixins.js
+++ misc/mixins.js
@@ -269,3 +269,15 @@ export const rgbToHex = ( r, g, b ) => {
 
 
 
+// convert HEX to RGB
+export const hexToRgb = ( hex ) => {
+	hex = hex.replace( /^#/, '' )
+	const bigint = parseInt( hex, 16 )
+	const r = ( bigint >> 16 ) & 255
+	const g = ( bigint >> 8 ) & 255
+	const b = bigint & 255
+	return { r, g, b }
+}
+
+
+
