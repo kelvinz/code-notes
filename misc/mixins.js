38af2479b0728a9abc66cf7555e80d627ad7e121
kelvin zhao <takingovertheworld@gmail.com>
2024-10-18 08:04:17 +0800

    ✨ 241018

--- misc/mixins.js
+++ misc/mixins.js
@@ -281,3 +281,14 @@ export const hexToRgb = ( hex ) => {
 
 
 
+// generate a random UUID (v4 uuid standard)
+export const generateUUID = () => {
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace( /[xy]/g, ( c ) => {
+		const r = Math.random() * 16 | 0
+		const v = c === 'x' ? r : ( r & 0x3 | 0x8 )
+		return v.toString( 16 )
+	} )
+}
+
+
+
