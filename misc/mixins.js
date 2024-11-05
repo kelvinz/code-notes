c1e4b7322f5629ef42c45f0285cfb628376f134b
kelvin zhao <takingovertheworld@gmail.com>
2024-11-06 07:53:18 +0800

    ✨ 241106

--- misc/mixins.js
+++ misc/mixins.js
@@ -403,3 +403,20 @@ export const verifySignature = async ( message, signature, publicKey ) => {
 }
 
 
+
+// hash a message
+export const hashMessage = async ( message ) => {
+	const encoder = new TextEncoder()
+	const data = encoder.encode( message )
+
+	const hashBuffer = await window.crypto.subtle.digest( 'SHA-256', data )
+
+	// convert buffer to hex string
+	const hashArray = Array.from( new Uint8Array( hashBuffer ) )
+	const hashHex = hashArray.map( b => b.toString( 16 ).padStart( 2, '0' ) ).join( '' )
+
+	return hashHex
+}
+
+
+
