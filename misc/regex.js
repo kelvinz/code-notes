415dc10bcc259f8521d2f98e5e9da99f947bc446
kelvin zhao <takingovertheworld@gmail.com>
2024-11-15 09:41:55 +0800

    ✨ 241115

--- misc/regex.js
+++ misc/regex.js
@@ -3,3 +3,9 @@ RegEx Patterns
 Most useful patterns for daily coding
 =====================*/
 
+/*========== Email Validation ==========*/
+// Perfect balance between validation and real-world emails
+const emailRegex = /^[\w@]+@[\w@]+\.[^\s@]+$/
+"user@domain.com".match( emailRegex )		// ✓
+"invalid.email@Com".match( emailRegex )		// ✗
+
