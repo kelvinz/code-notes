f37121cc6ac9f800b61ac105a3137471eeb1dd31
kelvin zhao <takingovertheworld@gmail.com>
2024-12-15 18:29:18 +0800

    ✨ 241215

--- misc/mixins.js
+++ misc/mixins.js
@@ -524,3 +524,17 @@ export const isTouchDevice = () => {
 
 
 
+// median of an array of numbers
+export const getMedian = ( numbers ) => {
+	const sorted = [ ...numbers ].sort( ( a, b ) => a - b )
+	const middle = Math.floor( sorted.length / 2 )
+
+	if ( sorted.length % 2 === 0 ) {
+		return ( sorted[ middle - 1 ] + sorted[ middle ] ) / 2
+	}
+
+	return sorted[ middle ]
+}
+
+
+
