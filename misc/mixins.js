2f9ac9afceb96596eda9753118405c50f88fb0f5
kelvin zhao <takingovertheworld@gmail.com>
2024-12-19 21:30:05 +0800

    ✨ 241219

--- misc/mixins.js
+++ misc/mixins.js
@@ -566,3 +566,10 @@ export const toSnakeCase = ( str ) => {
 
 
 
+// convert a string to title case
+export const toTitleCase = ( str ) => {
+	return str.replace( /\w\S*/g, ( txt ) => txt.charAt( 0 ).toUpperCase() + txt.substr( 1 ).toLowerCase() )
+}
+
+
+
