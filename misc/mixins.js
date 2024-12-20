aa4e8e271d33e4d059596bb4b60816cad4a6d26b
kelvin zhao <takingovertheworld@gmail.com>
2024-12-20 08:57:16 +0800

    ✨ 241220

--- misc/mixins.js
+++ misc/mixins.js
@@ -573,3 +573,15 @@ export const toTitleCase = ( str ) => {
 
 
 
+// generate a random string of specified length
+export const generateRandomString = ( length ) => {
+	const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+	let result = ''
+	for ( let i = 0; i < length; i++ ) {
+		result += chars.charAt( Math.floor( Math.random() * chars.length ) )
+	}
+	return result
+}
+
+
+
