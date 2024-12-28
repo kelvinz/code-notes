af77f5afccdc31d4c408516df9c561374c4cfef5
kelvin zhao <takingovertheworld@gmail.com>
2024-12-28 12:23:04 +0800

    ✨ 241228

--- misc/mixins.js
+++ misc/mixins.js
@@ -638,3 +638,10 @@ export const removeFalsyValues = ( arr ) => {
 
 
 
+// calculate factorial of a number
+export const factorial = ( n ) => {
+	if ( n <= 1 ) return 1
+	return n * factorial( n - 1 )
+}
+
+
